e73e6f5f7f80ef6a213c4f39bb8b59007820f67b
Marcos Meneghetti <meneghettimarcos@outlook.com>
2024-09-22 09:53:21 +0200

    FRONT-END shop filter  page

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -8,8 +8,15 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ShopController extends AbstractController
 {
+  #[Route('/shop', name: 'shop')]
+  public function index(): Response
+  {
+    return $this->render('pages/shop.html.twig');
+  }
+
+
   #[Route('/shop/product/{slug}', name: 'shop_product', requirements: ['slug' => '[a-zA-Z0-9-_]+'])]
-  public function index(string $slug): Response
+  public function show(string $slug): Response
   {
     return $this->render('products/show.html.twig');
   }
