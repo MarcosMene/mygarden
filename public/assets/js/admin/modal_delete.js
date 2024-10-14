d06d4b5843f3cac0b7e03ddd8f5501b06c34b910
Marcos Meneghetti <meneghettimarcos@outlook.com>
2024-10-14 10:33:05 +0200

    FRONT-END FIX modal delete

--- public/assets/js/admin/modal_delete.js
+++ public/assets/js/admin/modal_delete.js
@@ -1,20 +1,22 @@
-let teste = document.getElementById('cancel-button')
+if (typeof cancelButton === 'undefined') {
+  const cancelButton = document.getElementById('cancel-button')
 
-// Function to open the delete modal
-function openDeleteModal(productId, productName) {
-  // Show the modal
-  document.getElementById('modal_delete').classList.remove('hidden')
+  // Function to open the delete modal
+  function openDeleteModal(productId, productName) {
+    // Show the modal
+    document.getElementById('modal_delete').classList.remove('hidden')
 
-  // Update the product name in the modal
-  document.getElementById('delete-product-name').textContent = productName
+    // Update the product name in the modal
+    document.getElementById('delete-product-name').textContent = productName
 
-  // Add click event to confirm delete button
-  document.getElementById('confirm-button').onclick = function () {
-    // Submit the delete form for the selected product
-    document.getElementById('delete-form-' + productId).submit()
+    // Add click event to confirm delete button
+    document.getElementById('confirm-button').onclick = function () {
+      // Submit the delete form for the selected product
+      document.getElementById('delete-form-' + productId).submit()
+    }
   }
+  // Function to close the delete modal
+  cancelButton.addEventListener('click', () => {
+    document.getElementById('modal_delete').classList.add('hidden')
+  })
 }
-// Function to close the delete modal
-teste.addEventListener('click', () => {
-  document.getElementById('modal_delete').classList.add('hidden')
-})
